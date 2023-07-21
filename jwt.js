2a60cc2d69610a46598a4234a32d1d6603115be6
Nileshpataskar <nileshpataskars@gmail.com>
2023-07-21 17:47:49 +0530

    fourth

--- jwt.js
+++ jwt.js
@@ -1,16 +1,17 @@
 const jwt=require("jsonwebtoken")
 const dotenv = require('dotenv');
 dotenv.config();
+const SECRET_KEY="Nilesh"
 
 function createToken(data, expiresIn){
-    const token = jwt.sign(data, process.env.SECRET_KEY, { expiresIn });
+    const token = jwt.sign(data, SECRET_KEY, { expiresIn });
     return token
 
 }
 
 function verifyToken(token) {
     try {
-        const tokenData = jwt.verify(token,  process.env.SECRET_KEY);
+        const tokenData = jwt.verify(token, SECRET_KEY);
         return tokenData;
     } catch (err) {
         console.log(err);
